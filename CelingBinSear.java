f6d7831f73de1311030319a7bd8454982deafeac
Loke Varma <lokeshvarma4511@gmail.com>
2022-08-15 10:27:45 +0530

    commit

--- CelingBinSear.java
+++ CelingBinSear.java
@@ -5,12 +5,22 @@ public class CelingBinSear
     public static void main(String[] args)
     {
         int[] arr={3,7,9,12,19,24,28,32,38,40,43,62,89,99};
-        int tg=20;
+        int tg=235;
         System.out.println(ceilSearch(arr,tg));
     }
     // returns the smallest number which is greater than or equal to target
     static int ceilSearch(int[] arr, int tg)
     {
+        // returns -1 if the target number is greater than greater element in array
+        if(tg>=arr[arr.length-1])
+        {
+            return -1;
+        }
+        //returns index 0 if the target element is lesser than smallest number in the array
+        if(tg<=arr[0])
+        {
+            return 0;
+        }
         int start=0;
         int end=arr.length;
         if(arr.length<=0)
--- FloorBinSear.java
+++ FloorBinSear.java
@@ -2,12 +2,22 @@ package company.dsa;
 public class FloorBinSear {
     public static void main(String[] args) {
         int[] arr={12,15,19,26,39,43,47,49,50,60,70,80};
-        int tg=30;
+        int tg=88;
         System.out.println(floorSearch(arr,tg));
     }
     // returns the smallest number which is lesser than or equal to target
     static int floorSearch(int[]arr,int tg)
     {
+        // returns the smallest number if the target number is lesser then smallest number in array
+        if(tg<=arr[0])
+        {
+            return -1;
+        }
+        //returns the greatest number if the target number is greater then greatest number in the array
+        if(tg>=arr[arr.length-1])
+        {
+            return tg;
+        }
         if(arr.length==0)
         {
             return -1;
