aabbef5d232180dbcd2837aea331d6f4a6647b97
Loke Varma <lokeshvarma4511@gmail.com>
2022-08-18 22:40:39 +0530

    commit

---
+++ SelectionSort.java
@@ -0,0 +1,28 @@
+package company.dsa;
+import java.util.Arrays;
+public class SelectionSort {
+
+    public static void main(String[] arg)
+    {
+        int[] arr={67,34,100,-235,9,0,-199};
+        //function calling
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+    //selection sort algorithm function defining
+    static boolean[] selectionSort(int [] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+           //max item from remaining array
+            int end=arr.length-1-i;
+           int max=max(arr,0,end);
+        }
+
+
+        return new boolean[0];
+    }
+    static int max(int[] arr, int start, int end)
+    {
+        return start;
+    }
+}
