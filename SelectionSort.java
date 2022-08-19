1ef0ccbc3dceaa18c19fa0fd7d3363f895a54323
Loke Varma <lokeshvarma4511@gmail.com>
2022-08-19 15:01:04 +0530

    commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,29 +1,50 @@
 package company.dsa;
 import java.util.Arrays;
-public class SelectionSort {
 
-    public static void main(String[] arg)
+public class SelectionSort {
+    public static void main(String[] sar)
     {
-        int[] arr={67,34,100,-235,9,0,-199};
-        //function calling
+        int[] arr = {67, 34, 100, -235, 9, 0, -199};
         System.out.println(Arrays.toString(selectionSort(arr)));
     }
-    //selection sort algorithm function defining
-    static boolean[] selectionSort(int [] arr)
+    static int[] selectionSort(int[] arr)
     {
+        int end=arr.length;
+        /*
+        selection sort only need one iteration
+         */
         for(int i=0;i<arr.length-1;i++)
         {
-           //max item from remaining array
-            int end=arr.length-1-i;
-           int max=max(arr,0,end);
+            //end for decreasing the array after every iteration
+            end--;
+            //for finding max element between the indexes
+            int max=max(arr,0,end);
+            //swapping max index element  and end index element of the specified array size
+            swap(arr,max,end);
         }
-
-
-        return new boolean[0];
+        return arr;
     }
-    static int max(int[] arr, int start, int end)
-    {
-        return start;
 
+    //swapping
+    static void swap(int[] arr,int max,int end) {
+        int temp=arr[max];
+        arr[max]=arr[end];
+        arr[end]=temp;
+    }
+    //function for finding max element in remaining array
+    static int max(int[] arr,int start,int end)
+    {
+        int maxEle = Integer.MIN_VALUE;
+        int maxindex=-1;
+        for(int i=0;i<end;i++)
+        {
+            if(arr[i]> maxEle)
+            {
+                maxEle =arr[i];
+                maxindex=i;
+
+            }
+        }
+        return maxindex;
     }
 }
