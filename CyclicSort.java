a97e27eb54d1c315a0487523aadf41093bd3a2f1
Loke Varma <lokeshvarma4511@gmail.com>
2022-08-24 22:46:36 +0530

    commit

--- CyclicSort.java
+++ CyclicSort.java
@@ -1,29 +1,48 @@
 package company.dsa;
 import java.util.Arrays;
-public class CyclicSort
-{
-    public static void main(String[] a)
-    {
-        int[] arr = {3, 5, 2, 1, 4};
+public class CyclicSort {
+    public static void main(String[] a) {
+        int[] arr = {3, 5, 4, 1, 2};
         System.out.println(Arrays.toString(cyclicSort(arr)));
     }
-    static int[] cyclicSort(int[] arr)
-    {
-        //starts from element 0
-        for(int i=0;i<arr.length-1;i++)
+
+    static int[] cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length - 1)
         {
-            //checks whether the ith element value is i+1 or not
-            if(arr[i]!=i+1)
-            {
-                // if not, swaps the element to  element value -1 index < -- >
-                int temp=arr[i]-1;
-                int k=arr[temp];
-                arr[temp]=arr[i];
-                arr[i]=k;
-                i=-1;
+            if (arr[i]!=i+1) {
+            //swapping
+                int a=arr[i]-1;
+                int b=arr[a];
+                int c=arr[i];
+                arr[i]=b;
+                arr[a]=c;
             }
-            // else the loop continues
+            else
+            {
+                i++;
+            }
         }
         return arr;
     }
 }
+
+
+//    {
+//        //starts from element 0
+//        for(int i=0;i<arr.length-1;i++)
+//        {
+//            //checks whether the ith element value is i+1 or not
+//            if(arr[i]!=i+1)
+//            {
+//                // if not, swaps the element to  element value -1 index < -- >
+//                int temp=arr[i]-1;
+//                int k=arr[temp];
+//                arr[temp]=arr[i];
+//                arr[i]=k;
+//                i=-1;
+//            }
+//            // else the loop continues
+//        }
+//        return arr;
+//    }
