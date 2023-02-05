c85eb249624a0d61f366555c4aee02389f2a1197
Loke Varma <lokeshvarma4511@gmail.com>
2023-02-06 01:29:18 +0530

    commit

--- JavaBasics/XORRange.java
+++ JavaBasics/XORRange.java
@@ -3,8 +3,24 @@ package company.dsa.JavaBasics;
 public class XORRange
 {
     public static void main(String[] args) {
-        int a=4;
+        int a=3;
+//        System.out.println(rangeXOR(7));
 
-
+        int b=9 ;
+        // Range between a and b
+        int ans = rangeXOR(a) ^ rangeXOR(b);
+        System.out.println(ans);
+    }
+    private static int rangeXOR(int n) {
+        if (n % 4 == 0) {
+            return n;
+        }
+        if (n % 4 == 1) {
+            return 1;
+        }
+        if (n % 4 == 2) {
+            return n+1;
+        }
+        return 0;
     }
 }
